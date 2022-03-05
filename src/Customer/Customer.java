573f9f715b21e3dad847ed5c3929c0c30d1c56e3
Ryan Sim <Ryanyhsim@gmail.com>
2022-03-05 23:16:15 +0800

    Created Customer.Java and .gitignore
    Customer.java
    -Created Constructor, Getter and Setters for the class

---
+++ src/Customer/Customer.java
@@ -0,0 +1,63 @@
+package Customer;
+
+import java.util.LinkedList;
+
+public class Customer {
+    enum Status
+    {
+        Normal,
+        Privilleged
+    }
+    private int CustomerID;
+    static private int IDTracker=0;
+    private String Name,Email,Phone;
+    private Enum<Status> CustomerStatus;
+    //private List<Account> AccountList;
+
+    public Customer(String Name, String Email, String Phone)
+    {
+        this.CustomerID= IDTracker;
+        IDTracker+=1;
+        this.Name = Name;
+        this.Email = Email;
+        this.Phone = Phone;
+        this.CustomerStatus = Status.Normal;
+        //this.AccountList = new LinkedList<Account>();
+    }
+
+    public int getCustomerID() {
+        return CustomerID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public Enum<Status> getCustomerStatus() {
+        return CustomerStatus;
+    }
+
+    public void setCustomerStatus(Enum<Status> customerStatus) {
+        CustomerStatus = customerStatus;
+    }
+}
