f49900751a50486887f2f2db86bfd32ce13f8bee
Lua Zhi Zhan <zhizhanlua@gmail.com>
2022-03-20 15:55:50 +0800

    Add test cases for Customer and Address Classes

--- app/src/main/java/Customer/Customer.java
+++ app/src/main/java/Customer/Customer.java
@@ -1,17 +1,14 @@
 package Customer;
 
-import java.util.LinkedList;
 import java.util.UUID;
 
 public class Customer {
 
     private UUID customerID;
-    private String name,email,phone;
+    private String name, email, phone;
     private Address address;
 
-
-    public Customer(String name, String email, String phone)
-    {
+    public Customer(String name, String email, String phone) {
         this.name = name;
         setEmail(email);
         setPhone(phone);
@@ -33,17 +30,16 @@ public class Customer {
         return email;
     }
 
-    public void setEmail(String email)
-    {
-        String emailValid = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
-        if (email.matches(emailValid))
-        {
+    public void setEmail(String email) {
+        // https://www.baeldung.com/java-email-validation-regex#strict-regular-expression-validation
+        String emailValid = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
+        if (email.matches(emailValid)) {
             this.email = email;
 
-        }
-        else
-        {
-            throw new IllegalArgumentException("Invalid Email");
+        } else {
+            throw new IllegalArgumentException("Invalid Email.");
         }
 
     }
@@ -52,17 +48,21 @@ public class Customer {
         return this.phone;
     }
 
-    public void setPhone(String phone)
-    {
-        String phoneValid = "^(1\\-)?[0-9]{3}\\-?[0-9]{3}\\-?[0-9]{4}$";
-        if(phone.matches(phoneValid))
-        {
+    public void setPhone(String phone) {
+        // +65 optional, first digit 8/9, 7 digits between 0 - 9
+        String phoneValid = "(\\+?65)?(8|9)[0-9]{7}";
+        if (phone.matches(phoneValid)) {
             this.phone = phone;
-        }
-        else
-        {
-            throw new IllegalArgumentException("Invalid Phone Number");
+        } else {
+            throw new IllegalArgumentException("Invalid Phone Number.");
         }
     }
 
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
