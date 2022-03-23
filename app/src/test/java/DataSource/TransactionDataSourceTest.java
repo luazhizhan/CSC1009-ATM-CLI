827715d5c35a2b529c3b2ace52def51f7d20a934
Lua Zhi Zhan <zhizhanlua@gmail.com>
2022-03-23 10:19:40 +0800

    Fix test case

--- app/src/test/java/DataSource/TransactionDataSourceTest.java
+++ app/src/test/java/DataSource/TransactionDataSourceTest.java
@@ -15,8 +15,8 @@ public class TransactionDataSourceTest {
     @Test
     public void success() throws FileNotFoundException, IOException {
         DataSource<Transaction> txnDataSource = new TransactionDataSource();
-        Transaction cashTxn = txnDataSource.getDataById("41275c16-bead-4e90-af09-d1de693a2284");
-        Transaction tranfersTxn = txnDataSource.getDataById("553c960b-0200-43dd-a802-e08d91d02a19");
+        Transaction cashTxn = txnDataSource.getDataById("41275c16bead4e90af09d1de693a2284");
+        Transaction tranfersTxn = txnDataSource.getDataById("553c960b020043dda802e08d91d02a19");
         assertEquals(cashTxn.getAmount(), new BigDecimal(70));
         assertEquals(tranfersTxn.getAmount(), new BigDecimal(60));
     }
