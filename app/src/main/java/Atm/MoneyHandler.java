f8f9af2234aebf50cffeb1e064eaddae346b70ac
enriqueSIT <2102740@sit.singaporetech.edu.sg>
2022-03-28 18:39:02 +0800

    added algo to moneyHandler, deleted algo class

--- app/src/main/java/Atm/MoneyHandler.java
+++ app/src/main/java/Atm/MoneyHandler.java
@@ -58,7 +58,7 @@ public class MoneyHandler {
 
     // Withdraw this value.
     public Tuple<BigDecimal, int[]> withdraw(int value) throws IllegalArgumentException {
-        Tuple<Boolean, int[]> dispensed = ChangeAlgo.MakeChange(dispenserAmounts[0], dispenserAmounts[1], value);
+        Tuple<Boolean, int[]> dispensed = MakeChange(dispenserAmounts[0], dispenserAmounts[1], value);
         Tuple<BigDecimal, int[]> returnValue = new Tuple<BigDecimal, int[]>(BigDecimal.ZERO, dispensed.y);
         try {
             if (value < withdrawMinimum)
@@ -127,6 +127,40 @@ public class MoneyHandler {
             totalValue += notes[i] * dispenserAmounts[0][i];
         }
         return totalValue;
+    }
+    
+    // Algorithm for making change, given an array of denominations, limits per each denomination, and a value.
+    private Tuple<Boolean, int[]> MakeChange(int[] denominations, int[] limits, int value) {
+
+        if (limits.length != denominations.length) {
+            // We have an error. Sizes do not match. Print error and exit.
+            System.err.println("Denominations and Limits list must be of the same size.");
+            System.exit(1);
+        }
+
+        int[] values = new int[denominations.length];
+
+        for (int i = denominations.length - 1; i >= 0; i--) {
+            if (value == 0)
+                values[i] = 0;
+            else {
+                int denomination = denominations[i];
+                int sub = value / denomination;
+                int current = limits[i];
+
+                sub = Math.min(current, sub);
+                value -= denomination * sub; // updating the value.
+                values[i] = sub; // Set to sub to get the bills used.
+            }
+        }
+
+        if (value != 0) {
+            return new Tuple<Boolean, int[]>(false, values);
+        }
+
+        // Everything works. Return the values.
+        return new Tuple<Boolean, int[]>(true, values);
+
     }
 
 }
